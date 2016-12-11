bbf9044c3db5ded884634f3e085acbe233a61e7d
LSJ <fewfree12@naver.com>
2016-12-11 16:20:35 +0900

    init

---
+++ unity_project/ARTest/Assets/ARFINALPROJECT/Script/Explosion.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+    public GameObject house;
+    public GameObject explosion;
+    public GameObject fire;
+
+    private Object booming;
+    private Object burning;
+    private bool check_first = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter(Collider Col)
+    {
+        if (Col.tag == "Bar")
+        {
+            if (check_first)
+            {
+                booming = Instantiate(explosion, house.transform.position, house.transform.rotation);
+                burning = Instantiate(fire, house.transform.position, house.transform.rotation);
+
+                Destroy(booming, 3f);
+                Destroy(burning, 5f);
+
+                check_first = false;
+            }
+        }
+    }
+}
---
+++ unity_project/ARTest/Assets/CollisionCheck.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionCheck : MonoBehaviour {
+
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerStay(Collider Col)
+    {
+        if (Col.tag == "Bar")
+        {
+            //event -> success
+            Debug.Log("Collision Success: BAR");
+            anim.SetBool("Next", true);
+        }
+    }
+}
